24f7c9bc4a4b5610d981148c03c11e52ce045c04
howard8082 <howard8082@users.noreply.github.com>
2017-09-15 11:33:05 +0800

    修改，跟着第一次，会有啥呢？
    Signed-off-by: howard8082 <howard8082@users.noreply.github.com>

--- jackdance/src/com/sta/a/HelloWorld.java
+++ jackdance/src/com/sta/a/HelloWorld.java
@@ -1,7 +1,19 @@
 package com.sta.a;
 
 public class HelloWorld {
-    public HelloWorld() {
-        // ?
+	
+	private String saySome;
+	
+    public HelloWorld(String word) {
+        this.saySome = word;
     }
+
+	public String getSaySome() {
+		return saySome;
+	}
+
+	public void setSaySome(String saySome) {
+		this.saySome = saySome;
+	}
+    
 }
